508acb6e5ea9cf17e2d5795c287b7b24f05952b7
Omer Hen <omer1342@gmail.com>
2019-12-26 04:11:30 +0200

    Added documentation, visualation at end of learning process, and refined README file.

--- Final Network.py
+++ Final Network.py
@@ -1,22 +1,26 @@
+'''
+AUTHOR: Omer Hen
+PURPOSE: Final Computer Science Project, 2019, Shimon Ben Zvi High School
+
+This project covers the implementation and research into ANNs.
+'''
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pickle, gzip, urllib.request, json
-import numpy as np
 import os.path
-
-np.random.seed(1) # set a seed so that the results are consistent
-
 
 
 def layer_sizes(X, Y):
     """
+    Calculates the required number of neurons in the input and output layers.
+
     Arguments:
     X -- input dataset of shape (number of examples, input size)
     Y -- labels of shape (number of examples, output size)
 
     Returns:
     n_x -- the size of the input layer
-    n_h -- the size of the hidden layer
     n_y -- the size of the output layer
     """
     n_x = X.shape[0] 
@@ -27,17 +31,19 @@ def layer_sizes(X, Y):
 
 def initialize_parameters(n_x, n_h, n_y):
     """
+    Creates the initial weights and biases for the layers of the network.
+
     Argument:
     n_x -- size of the input layer
     n_h -- size of the hidden layer
     n_y -- size of the output layer
 
     Returns:
-    params -- python dictionary containing your parameters:
-                    W1 -- weight matrix of shape (n_h, n_x)
-                    b1 -- bias vector of shape (n_h, 1)
-                    W2 -- weight matrix of shape (n_y, n_h)
-                    b2 -- bias vector of shape (n_y, 1)
+    params -- python dictionary containing the network's parameters:
+            W1 -- weight matrix of shape (n_h, n_x)
+            b1 -- bias vector of shape (n_h, 1)
+            W2 -- weight matrix of shape (n_y, n_h)
+            b2 -- bias vector of shape (n_y, 1)
     """
     W1 = np.random.randn(n_h, n_x)*0.01
     b1 = np.zeros((n_h, 1))
@@ -54,23 +60,26 @@ def initialize_parameters(n_x, n_h, n_y):
 
 def forward_propagation(X, parameters):
     """
+    Mimics the Feedforward Propagation of a neural network. 
+
     Argument:
-    X -- input data of size (n_x, m)
-    parameters -- python dictionary containing your parameters (output of initialization function)
+    X -- input data of size (784, number of examples)
+    parameters -- python dictionary containing the network's parameters: W1, b1, W2 and b2 (output of initialization function)
 
     Returns:
     A2 -- The sigmoid output of the second activation
     cache -- a dictionary containing "Z1", "A1", "Z2" and "A2"
     """
+    # Retrieve the weights and biases from "parameters"
     W1 = parameters["W1"]
     b1 = parameters["b1"]
     W2 = parameters["W2"]
     b2 = parameters["b2"]
 
-    Z1 = np.dot(W1, X) + b1
-    A1 = np.tanh(Z1)
+    Z1 = np.dot(W1, X) + b1     # Calculation of the weighted sum plus the bias
+    A1 = np.tanh(Z1)            # tanh activation function in the hidden layer
     Z2 = np.dot(W2, A1) + b2
-    A2 = sigmoid(Z2)
+    A2 = sigmoid(Z2)            # sigmoid activation function in the output layer 
 
     cache = {"Z1": Z1,
              "A1": A1,
@@ -82,24 +91,24 @@ def forward_propagation(X, parameters):
 
 def compute_cost(A2, Y, parameters):
     """
-    Computes the cost(cross- entropy cost) given in the equation above.
+    Computes the cost, according to the cross-entropy equation.
 
     Arguments:
-    A2 -- The sigmoid output of the second activation, of shape (1, number of examples) / (number of examples, 1)
-    Y -- "true" labels vector of shape (1, number of examples)                          / (number of examples, 1)
-    parameters -- python dictionary containing your parameters W1, b1, W2 and b2
+    A2 -- The sigmoid output of the second activation, of shape (10, number of examples) 
+    Y -- "true" labels vector, or simply the target values of shape (10, number of examples)                          
+    parameters -- python dictionary containing the network's parameters: W1, b1, W2 and b2
 
     Returns:
-    cost --cross-entropy cost
+    cost -- cross-entropy cost
     """
 
-    m = Y.shape[1] # number of examples
+    m = Y.shape[1] # Number of examples
 
-    # Compute cost
+    # Compute cost according to the cross-entropy equation
     logprobs = np.multiply(np.log(A2), Y) + np.multiply((1-Y), np.log(1- A2))
     cost = -np.sum(logprobs)/m
     
-    cost = np.squeeze(cost)     # makes sure cost is the dimension we expect.
+    cost = np.squeeze(cost)     # makes sure cost is the dimension we expect
                                 # E.g., turns [[17]] into 17
 
     return cost
@@ -107,28 +116,28 @@ def compute_cost(A2, Y, parameters):
 
 def backward_propagation(parameters, cache, X, Y):
     """
-    Implement the backward propagation using the instructions above.
+    Implements Backward Propagation for the entire network.
 
     Arguments:
-    parameters -- python dictionary containing our parameters 
-    cache -- a dictionary containing "Z1", "A1", "Z2" and "A2".
-    X -- input data of shape (2, number of examples)
-    Y -- "true" labels vector of shape (1, number of examples)
+    parameters -- python dictionary containing the network's parameters: W1, b1, W2 and b2 
+    cache -- a dictionary containing: "Z1", "A1", "Z2" and "A2".
+    X -- input data of shape (784, number of examples)
+    Y -- "true" labels vector, or simply the target values of shape (10, number of examples)  
 
     Returns:
-    grads -- python dictionary containing your gradients with respect to different parameters
+    nabla -- python dictionary containing the gradients with respect to different parameters
     """
-    m = X.shape[1]
+    m = X.shape[1] # Number of examples
 
-    # First, retrieve W1 and W2 from the dictionary "parameters".
+    # Retrieve "W1" and "W2" from the dictionary "parameters"
     W1 = parameters["W1"]
     W2 = parameters["W2"]
 
-    # Retrieve also A1 and A2 from dictionary "cache".
+    # Retrieve "A1" and "A2" from the dictionary "cache"
     A1 = cache["A1"]
     A2 = cache["A2"]
 
-    # Backward propagation: calculate dW1, db1, dW2, db2.
+    # Backward propagation: calculate dW1, db1, dW2, db2
     dZ2 = A2 - Y
     dW2 = np.dot(dZ2, A1.T)/m
     db2 = np.sum(dZ2, axis=1, keepdims = True) / m
@@ -137,39 +146,39 @@ def backward_propagation(parameters, cache, X, Y):
     dW1 = np.dot(dZ1, X.T)/m
     db1 = np.sum(dZ1, axis = 1, keepdims = True) / m
 
-    grads = {"dW1": dW1,
+    nabla = {"dW1": dW1,
              "db1": db1,
              "dW2": dW2,
              "db2": db2}
 
-    return grads
+    return nabla
 
 
-def update_parameters(parameters, grads, learning_rate = 0.35):
+def update_parameters(parameters, nabla, learning_rate):
     """
-    Updates parameters using the gradient accent update rule given above
+    Updates parameters using the Gradient Descent update rule.
 
     Arguments:
-    parameters -- python dictionary containing your parameters 
-    grads -- python dictionary containing your gradients 
+    parameters -- python dictionary containing the network's parameters: W1, b1, W2 and b2 
+    nabla -- python dictionary containing the network parameters' gradients
+    learning_rate -- the learning rate of the network, affects the impact the gradients will have on updated parameters
 
     Returns:
-    parameters -- python dictionary containing your updated parameters 
+    parameters -- python dictionary containing the updated parameters 
     """
-    # Retrieve each parameter from the dictionary "parameters"
+    # Retrieve the weights and biases from "parameters"
     W1 = parameters["W1"]
     b1 = parameters["b1"]
     W2 = parameters["W2"]
     b2 = parameters["b2"]
-    ### END CODE HERE ###
 
-    # Retrieve each gradient from the dictionary "grads"
-    dW1 = grads["dW1"]
-    db1 = grads["db1"]
-    dW2 = grads["dW2"]
-    db2 = grads["db2"]
+    # Retrieve the gradients from "nabla"
+    dW1 = nabla["dW1"]
+    db1 = nabla["db1"]
+    dW2 = nabla["dW2"]
+    db2 = nabla["db2"]
 
-    # Update rule for each parameter
+    # Update the network's parameters
     W1 -= learning_rate * dW1
     b1 -= learning_rate * db1
     W2 -= learning_rate * dW2
@@ -183,99 +192,207 @@ def update_parameters(parameters, grads, learning_rate = 0.35):
     return parameters
 
 
-def nn_model(X, Y, n_h, test_X, test_Y, num_iterations = 20000, print_cost=True):
+def nn_model(data, n_h, learning_rate = 0.35, epochs = 20000, print_cost=True):
     """
+    Main function, brings together the network.
+    
     Arguments:
-    X -- dataset of shape (2, number of examples)
-    Y -- labels of shape (1, number of examples)
+    data -- python dictionary containing the training and tetsing data:
+            X -- dataset of shape (784, number of examples)
+            Y -- labels of shape (10, number of examples)
+            test_X -- dataset of shape (784, number of examples)
+            test_Y -- labels of shape (10, number of examples)
     n_h -- size of the hidden layer
-    num_iterations -- Number of iterations in gradient descent loop
-    print_cost -- if True, print the cost every 1000 iterations
+    learning_rate -- the learning rate of the network, affects the impact the gradients will have on updated parameters
+    num_iterations -- Number of epochs
+    print_cost -- if True, print the cost every 100 iterations
 
     Returns:
-    parameters -- parameters learnt by the model. They can then be used to predict.
+    parameters -- parameters learnt by the model. They can then be used to predict 
     """
+    # Retrieve training and testing data from the dictionary "data"
+    X = data["X"]
+    Y = data["Y"]
+    test_X = data["test_X"]
+    test_Y = data["test_Y"]
 
-    np.random.seed(3)
     n_x = layer_sizes(X, Y)[0]
     n_y = layer_sizes(X, Y)[1]
 
-    # Initialize parameters, then retrieve W1, b1, W2, b2. Inputs: "n_x, n_h, n_y". Outputs = "W1, b1, W2, b2, parameters".
+    # Initialize parameters, then retrieve W1, b1, W2, b2
     parameters = initialize_parameters(n_x, n_h, n_y)
     W1 = parameters["W1"]
     b1 = parameters["b1"]
     W2 = parameters["W2"]
     b2 = parameters["b2"]
 
-    # Loop (gradient descent)
-    for i in range(0, num_iterations):
+    loss = []
+    accuracy = []
 
-        # Forward propagation. Inputs: "X, parameters". Outputs: "A2, cache".
+    # Loop (Gradient Descent)
+    for i in range(0, epochs):
+        
+        # First, forward propagate through the network 
         A2, cache = forward_propagation(X, parameters)
 
-        # Cost function. Inputs: "A2, Y, parameters". Outputs: "cost".
+        # Then, compute the cost of the network for its final output: "A2" and target values: "Y"
         cost = compute_cost(A2, Y, parameters)
 
-        # Backpropagation. Inputs: "parameters, cache, X, Y". Outputs: "grads".
-        grads = backward_propagation(parameters, cache, X, Y)
+        # Backpropagate through the layers of the network in order to achieve the gradients 
+        nabla = backward_propagation(parameters, cache, X, Y)
 
-        # Gradient descent parameter update. Inputs: "parameters, grads". Outputs: "parameters".
-        parameters = update_parameters(parameters, grads)
+        # Update the weights and biases according to the gradients
+        parameters = update_parameters(parameters, nabla, learning_rate)
 
+        if print_cost and i % 100 == 0:
+            precision = evaluate(test_X, test_Y, parameters)
+            print('Cost after iteration %i: %f, %a percent accuracy.' %(i, cost, precision))
+            loss.append(cost)
+            accuracy.append(precision)
 
-        # Print the cost every 1000 iterations
-        if print_cost and i % 1000 == 0:
-            print('Cost after iteration %i: %f, %a percent accuracy.' %(i, cost, round(evaluate(test_X, test_Y, parameters)*100, 3)))
-
+    visualize(loss, accuracy, epochs)
     return parameters
 
 
-
 def evaluate(X, y, parameters):
+    '''
+    Evaluates the network for its level of accuracy in testing data.
 
+    Arguments:
+    X -- dataset of shape (784, number of examples)
+    y -- labels of shape (10, number of examples)
+    parametes -- python dictionary containing the network's parameters: W1, b1, W2 and b2
+
+    Returns:
+    A rounded float describing the percentage of testing examples the network classified correctly
+    '''
+    # First, get the network output of the testing data
     o, cache = forward_propagation(X, parameters)
-    return (y.T.argmax(axis = 1) == o.T.argmax(axis = 1)).sum() / y.shape[1]
+    # Then, compare the network output to the target values 
+    return round(((y.T.argmax(axis = 1) == o.T.argmax(axis = 1)).sum() / y.shape[1] * 100), 3)
     
-
-
 
 def load_data():
-  file_name = 'mnist.pkl.gz'
+    '''
+    Loads the training and testing data from the MNIST databse.
 
-  if (not os.path.isfile(file_name)):
-    urllib.request.urlretrieve("http://deeplearning.net/data/mnist/" + file_name, file_name)
+    Arguments:
+    None.
+
+    Returns:
+    data -- python dictionary containing the training/testing examples and target values
+    '''
+    # File name of desired database
+    file_name = 'mnist.pkl.gz'
+
+    # If database file is not already downloaded, download it
+    if (not os.path.isfile(file_name)):
+        urllib.request.urlretrieve("http://deeplearning.net/data/mnist/" + file_name, file_name)
+
+    # Extract the training, testing and validating examples from the database
+    with gzip.open(file_name, 'rb') as file:
+        trs, vs, ts = pickle.load(file, encoding='latin1')
+
+    # Format the training and testing data in a proper manner - in order for the network to be able to interact with it
+    training_images = np.array(trs[0])
+    training_labels = np.array([vectorize(y) for y in trs[1]])
+
+    training_images, training_labels = randomize(training_images, training_labels) # Shuffle the data
+
+    test_images = np.array(ts[0])
+    test_labels = np.array(np.array([vectorize(y) for y in ts[1]]))
+
+    test_images, test_labels = randomize(test_images, test_labels)
     
-  with gzip.open(file_name, 'rb') as file:
-    trs, vs, ts = pickle.load(file, encoding='latin1')
-
-  training_images = np.array(trs[0])
-  training_labels = np.array([vectorize(y) for y in trs[1]])
-
-  test_images = np.array(ts[0])
-  test_labels = np.array(np.array([vectorize(y) for y in ts[1]]))
-
-
-  return training_images, training_labels, test_images, test_labels
+    data = {"X" : training_images,
+            "Y" : training_labels,
+            "test_X" : test_images,
+            "test_Y" : test_labels}
+    
+    return data
   
-def vectorize(k):
-  a = np.zeros(10)
-  a[k] = 1.0
-  return a
+def vectorize(j):
+    '''
+    Vectorizes an integer according to the output layer's requirements.
+
+    Arguments:
+    j -- An integer describing the index of the right answer of a certain target value
+
+    Returns:
+    a -- A vector describing the target value, based on j, for a certain example
+    '''
+    # Generates an empty array - all wrong answers are denoted wiht 0.0, and marks the right answer with 1.0
+    a = np.zeros(10)
+    a[j] = 1.0
+    return a
 
 
 def sigmoid(x):
+    '''
+    The sigmoid function.
+
+    Arguments:
+    x -- A certain value, or matrix to undergo the sigmoid function
+
+    Returns
+    The output of the sigmoid function for 'x'
+    '''
     return 1 / (1 + np.exp(-x))
 
 
+def randomize(x, y):
+    '''
+    Shuffles the arrays "x" and "y" in a random manner while keeping the
+    corresponding indices of the elements in arrays.
+
+    Arguments:
+    x -- Numpy Array of n dimensions
+    y -- Numpy array of n dimensions
+
+    Returns:
+    Transposed and shuffled "x" and "y"
+    '''
+    gamma = np.arange(x.shape[0])
+    np.random.shuffle(gamma)
+    return x[gamma].T, y[gamma].T
+
+
+def visualize(loss, evaluations, epochs):
+    '''
+    Plots the data collected during the training process of the network into graphs
+    displaying the cost and accuracy level of the network throughout the process.
+
+    Arguments:
+    loss -- Array containing the cost of the network over the course of the training process
+    evluations -- Array containing the accuracy level of the network over the course of the training process
+    epochs -- Number of epochs the network underwent
+
+    Returns:
+    None, produces graphs and saves them to the local folder
+    '''
+    xaxis = np.arange(0, epochs, 100)
+
+    plt.grid()
+    plt.xlabel('Epochs')
+    plt.ylabel('Cost')
+    plt.plot(xaxis, loss)
+
+    plt.savefig('CostGraph.png') # Save cost graph to local folder
+
+    plt.cla()
+    plt.grid()
+    plt.hlines(100, 0, epochs, linestyles='dashed') # Plot maximum line - at 100% success rate
+    plt.ylabel('Accuracy')
+    plt.plot(xaxis, evaluations)
+
+    plt.savefig('EvalGraph.png') # Save evluation graph to local folder
+
+
 if __name__ == "__main__":
-  images, labels, ts_images, ts_labels = load_data()
-  img = images[:45000].T
-  lbl = labels[:45000].T
-
-  tsimg = ts_images.T
-  tlbl = ts_labels.T
-
-  nn_model(img, lbl, 30, tsimg, tlbl)
+    # Retrieve the training and testing data
+    data = load_data()
+    # Calls the main method to start the learning proces
+    nn_model(data, 32)
 
 
 
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
+Final Computer Science Project, 2020 : Omer Hen, Shimon Ben Zvi High School 
+
+Description
+
+This project covers the research and implementation of an artificial neural network that recognizes hand written digits.
+The implementation of common concepts in this field of interest such as: Backpropagation, Feedfoward and Gradient Descent 
+is done without using third-party libraries, using only Numpy for fast and efficient linear algebra calculations.
+The training and testing data is imported from the MNIST database.
+
+Prerequisites
+
+Python 3 and the following libraries installted:
+
+ - Numpy 
+ - Matplotlib
+ - Picke
+ - gzip
+ - urllib
+ - json 
+ 
+
+Installation
+
+Download 'Final Network.py' and run it using a Python IDE
 
